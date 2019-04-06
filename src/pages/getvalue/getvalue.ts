23b67f84a2b5df2e51defe462bda5a35461ac795
Phakpoom Ittirattanakomon <beerbc16@gmail.com>
2019-04-06 17:05:42 +0700

    localstorage

---
+++ src/pages/getvalue/getvalue.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+/**
+ * Generated class for the GetvaluePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-getvalue',
+  templateUrl: 'getvalue.html',
+})
+export class GetvaluePage {
+  username:string;
+  studentId:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage:Storage) {
+    this.storage.ready().then(
+      ()=>{
+        this.storage.get('username').then(
+          (valName)=>{
+            this.username=valName;
+          }
+        );
+
+        this.storage.get('studentId').then(
+          (valId)=>{
+            this.studentId=valId;
+          }
+        );
+      }
+    );
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GetvaluePage');
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { GetvaluePage } from '../getvalue/getvalue';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public storage:Storage) {
+    storage.ready().then(
+      ()=>{
+        //set data to local storage
+        storage.set("username",'Phakpoom.I');
+        storage.set("studentId",'56130700071');
+      }
+    );
+  }
 
+  gotoGetValue(){
+    this.navCtrl.push(GetvaluePage);  
   }
 
 }
